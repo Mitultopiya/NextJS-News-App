2b95bf776b789ce1c0b5879397f750b16171045b
Mitul Topiya <mitultopiya308@gmail.com>
2026-07-03 15:33:18 +0530

    Updeted commit

---
+++ pages/eom.js
@@ -0,0 +1,45 @@
+import styles from "../styles/eom.module.css"
+import {Toolbar} from "../components/toolbar"
+
+export const EOM = ({employee}) => {
+    console.log(employee)
+
+    return (
+        <div className="page-container">
+            <Toolbar/>
+
+            <div className={styles.main}>
+                <h1 className={styles.employeeOfTheMonth}>Employee of The Month</h1>
+
+                <div className={styles.employeeOfTheMonth}>
+                    <h3>{employee.name}</h3>
+                    <h6>{employee.position}</h6>
+                    <img src={employee.image}/>
+                    <h6>
+                        <a href={employee.github} target="_blank" rel="noopener noreferrer">My Github</a>
+                    </h6>
+                    <p>{employee.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const getServerSideProps = async pageContext => {
+
+    const employee = {
+        name: "Pablo Felipe (PabloFLPs) 🦁",
+        position: "Web Developer",
+        github: "https://github.com/PabloFLPs",
+        image: "https://avatars.githubusercontent.com/u/58035837?v=4",
+        description: "For my family, I am the 'hackerman'."
+    }
+
+    return {
+        props: {
+            employee: employee
+        }
+    }
+}
+
+export default EOM
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import {Toolbar} from "../components/toolbar"
+
+export default function Home() {
+  return (
+    <div className="page-container">
+      <Head>
+          <meta property="og:image" content="https://raw.githubusercontent.com/PabloFLPs/NextJS-News-App/master/public/news-icon.png"/>
+          <meta name="description" content="A simple NextJS application to get the recent news from News API - Brazil located."/>
+      </Head>
+
+      <Toolbar/>
+
+      <div className={styles.main}>
+        <h1>Pablo FELPs - NextJS News App</h1>
+
+        <h3>Your one stop shop for the latest news articles</h3>
+        <p>
+          Project based on the PortEXE video:
+          <a href="https://www.youtube.com/watch?v=xtItzwYG6oQ"> Coding A News App With Next.js</a>
+        </p>
+      </div>
+    </div>
+  )
+}
